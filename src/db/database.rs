92eeb04f84599b3618dd7b1772f565090e80ac23
X0rD3v1L <sbenarjee7@gmail.com>
2024-02-26 15:18:48 +0530

    added env vars

--- src/db/database.rs
+++ src/db/database.rs
@@ -1,6 +1,8 @@
+use std::env;
 use surrealdb::engine::remote::ws::{Client, Ws};
 use surrealdb::opt::auth::Root;
 use surrealdb::{Error, Surreal};
+
 #[derive(Clone)]
 pub struct Database {
     pub client: Surreal<Client>,
@@ -11,14 +13,19 @@ pub struct Database {
 impl Database {
     // Initialize a new database connection
     pub async fn init() -> Result<Self, Error> {
+        dotenv::dotenv().ok();
+        // Retrieve database credentials from environment variables
+        let username = env::var("DB_USERNAME").expect("DB_USERNAME not set in environment");
+        let password = env::var("DB_PASSWORD").expect("DB_PASSWORD not set in environment");
+
         // Connect to the Surreal database engine via WebSocket
         let client = Surreal::new::<Ws>("127.0.0.1:8000").await?;
 
         // Sign in to the Surreal database with root credentials
         client
             .signin(Root {
-                username: "root",
-                password: "root",
+                username: &username,
+                password: &password,
             })
             .await?;
         client.use_ns("surreal").use_db("todoapp").await.unwrap();
